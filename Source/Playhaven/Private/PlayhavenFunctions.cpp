1cc6205ce27d4150f5cc1017d7342cfd4c727157
Robert Segal <rob@getsetgames.com>
2016-08-25 19:24:00 -0400

    Sets up iOS singleton to handle Playhaven iOS actions for application launch and entering foreground

--- Source/Playhaven/Private/PlayhavenFunctions.cpp
+++ Source/Playhaven/Private/PlayhavenFunctions.cpp
@@ -12,6 +12,92 @@
 
 
 #elif PLATFORM_IOS
+#import "PlayHavenSDK.h"
+#endif
 
+
+#if PLATFORM_IOS
+
+@interface PlayhavenFunctionsDelegate : NSObject<PHPublisherContentRequestDelegate>
+{
+}
+
+@end
+
+static PlayhavenFunctionsDelegate *phs;
+
+@implementation PlayhavenFunctionsDelegate
+
++(void)load
+{
+    if (!phs)
+    {
+        phs = [[PlayhavenFunctionsDelegate alloc] init];
+    }
+}
+
+-(id)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(applicationDidFinishLaunching:)
+                                                     name:UIApplicationDidFinishLaunchingNotification
+                                                   object:nil];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(applicationWillEnterForeground:)
+                                                     name:UIApplicationWillEnterForegroundNotification
+                                                   object:nil];
+    }
+    
+    return self;
+}
+
+-(void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+-(void)trackApplicationOpen
+{
+    NSDictionary *info = [phs infoSDKSettings];
+    
+    if (info)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[PHPublisherOpenRequest requestForApp:info[@"Token"] secret:info[@"Secret"]] send]
+        });
+    }
+    else
+    {
+        UE_LOG(LogPlayhaven, Log, TEXT("[Playhaven] was not found in info.plist tracking may not behave correctly"));
+    }
+}
+
+-(NSDictionary *)infoSDKSettings
+{
+    return [[NSBundle mainBundle] infoDictionary][@"Playhaven"];
+}
+
+-(void)applicationDidFinishLaunching:(NSNotification *)n
+{
+    NSDictionary *dLaunchOptionsUrl = n.userInfo[@"UIApplicationLaunchOptionsURLKey"];
+    
+    if (!dLaunchOptionsUrl)
+    {
+        [phs trackApplicationOpen];
+    }
+}
+
+-(void)applicationWillEnterForeground:(NSNotification *)n
+{
+    [phs trackApplicationOpen];
+}
+
+@end
 
 #endif
